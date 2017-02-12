a8c9fce666135d8242c381a2d988f4c15f3e814c
bedrin <Dmitry.Bedrin@gmail.com>
2017-02-12 18:55:48 +0300

    Merge remote-tracking branch 'source/master'
    # Conflicts:
    #	pom.xml
    #	readme.md
    #	src/main/resources/spring/data-access.properties
    #	src/main/resources/spring/mvc-core-config.xml
    #	src/main/webapp/WEB-INF/jsp/fragments/footer.jsp
    #	src/main/webapp/WEB-INF/jsp/fragments/staticFiles.jsp
    #	src/main/webapp/WEB-INF/jsp/welcome.jsp
    #	src/main/webapp/WEB-INF/web.xml

--- src/main/java/org/springframework/samples/petclinic/PetClinicApplication.java
+++ src/main/java/org/springframework/samples/petclinic/PetClinicApplication.java
@@ -16,16 +16,18 @@
 
 package org.springframework.samples.petclinic;
 
+import io.sniffy.boot.EnableSniffy;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
  * PetClinic Spring Boot Application.
- * 
+ *
  * @author Dave Syer
  *
  */
 @SpringBootApplication
+@EnableSniffy
 public class PetClinicApplication {
 
     public static void main(String[] args) throws Exception {
--- src/main/java/org/springframework/samples/petclinic/system/WelcomeController.java
+++ src/main/java/org/springframework/samples/petclinic/system/WelcomeController.java
@@ -1,14 +1,45 @@
 package org.springframework.samples.petclinic.system;
 
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.io.IOException;
 
 @Controller
 class WelcomeController {
 
-    @RequestMapping("/")
-    public String welcome() {
-        return "welcome";
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public ModelAndView homePage() throws IOException {
+
+        String article = null;
+        try {
+            article = getWikipediaArticle();
+        } catch (Exception e) {
+            article = e.getMessage();
+        }
+
+        ModelAndView modelAndView = new ModelAndView("welcome");
+        modelAndView.addObject("article", article);
+
+        return modelAndView;
     }
+
+    private String getWikipediaArticle() throws IOException {
+        RestTemplate restTemplate = new RestTemplate();
+        String wikipediaArticle = restTemplate.getForObject(
+            "https://en.wikipedia.org/w/api.php?action=parse&format=json&page=Pet&prop=text",
+            String.class);
+
+        ObjectMapper mapper = new ObjectMapper();
+        JsonNode jsonNode = mapper.readTree(wikipediaArticle);
+
+        return jsonNode.get("parse").get("text").get("*").asText();
+    }
+
 }
